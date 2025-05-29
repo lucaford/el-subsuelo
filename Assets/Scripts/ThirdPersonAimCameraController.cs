2ec6e92d30cd1357dd6bd0fa749385f46e9496a7
Luca Ford <lford@BA-MAC-211.local>
2025-05-29 18:15:51 -0300

    first commit

---
+++ Assets/Scripts/ThirdPersonAimCameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class ThirdPersonAimCameraController : MonoBehaviour
+{
+    [SerializeField] private CinemachineVirtualCamera aimVirtualCamera;
+    [SerializeField] private float normalSensitivity;
+    [SerializeField] private float aimSensitivity;
+
+    private ThirdPersonController thirdPersonController;
+    private StarterAssetsInputs starterAssetsInputs;
+
+    void Awake() {
+        starterAssetsInputs = GetComponent<StarterAssetsInputs>();
+    }
+
+    void Update()
+    {
+        if(starterAssetsInputs.aim)
+        {
+            aimVirtualCamera.gameObject.SetActive(true);
+            thirdPersonController.SetSensitivity(aimSensitivity);
+        }
+        else
+        {
+            aimVirtualCamera.gameObject.SetActive(false);
+            thirdPersonController.SetSensitivity(normalSensitivity);
+        }
+    }
+}
